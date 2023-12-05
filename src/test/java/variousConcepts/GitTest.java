89f306cb48a3e18b6089985dc99322bdab48d9b1
Mahrooz Hussain <ershad710@hotmail.com>
2023-12-04 18:44:54 -0600

    2nd commit

---
+++ src/test/java/variousConcepts/GitTest.java
@@ -0,0 +1,17 @@
+package variousConcepts;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		printMe();
+			}
+
+public static void printMe() { 
+	
+	System.out.println("Git Test");
+}
+
+
+}
---
+++ src/test/java/variousConcepts/GitTest2.java
@@ -0,0 +1,33 @@
+package variousConcepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		printMe();
+		printMe1();
+		printMe2();
+		printMe3();
+
+	}
+
+public static void printMe() { 
+	
+	System.out.println("Git Test");
+}
+
+public static void printMe1() { 
+	
+	System.out.println("Git Test1");
+}
+public static void printMe2() { 
+	
+	System.out.println("Git Test2");
+}	
+public static void printMe3() { 
+	
+	System.out.println("Git Test3");
+}
+
+}
